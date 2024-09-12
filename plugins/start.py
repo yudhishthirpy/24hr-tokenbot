02e4704f0d7b35fc5cee0232e203a1277fff00a9
brainsaga2006 <brainsaga2006@proton.me>
2024-09-12 16:00:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,8 +5,10 @@
 import asyncio
 from datetime import datetime
 from time import time
+
 import pyrogram.utils
-pyrogram.utils.MIN_CHANNEL_ID = -1002214972491
+pyrogram.utils.MIN_CHANNEL_ID = your channel id 
+
 from bot import Bot
 from config import (
     ADMINS,
